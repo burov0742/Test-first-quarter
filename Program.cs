a6b5c62ddbae7ad6e6afb776dc19fd52ca47433e
Valentin <burov0742@gmail.com>
2022-07-28 15:09:16 +0300

    ShowArray method added

--- Program.cs
+++ Program.cs
@@ -3,3 +3,12 @@
 string [] NewArray = {"Russia", "MSK", "USA", "France", "LA"};
 
 
+void ShowArray (string[] array)
+{
+    for (int i =0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+ShowArray(NewArray);
